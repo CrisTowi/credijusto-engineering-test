007f1ff95fc9c69d611980e380c0e26841fdc10f
Christian Consuelo <cconsuelo@MacBook-Air-de-Christian.local>
2018-10-26 16:29:19 -0500

    Added comments on helpers file

--- Helpers/helpers.js
+++ Helpers/helpers.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 
+// Format Fixer data to a valid format to store
 const formatFixer = (data) => {
   const splittedDate = data.date.split("-");
 
@@ -12,6 +13,8 @@ const formatFixer = (data) => {
   };
 }
 
+
+// Format Diario Oficial data to a valid format to store
 const formatDiarioOficial = (value, date) => {
   const splittedDate = date.split("/");
 
@@ -21,6 +24,7 @@ const formatDiarioOficial = (value, date) => {
   };
 }
 
+// Format Banxico data to a valid format to store
 const formatBanxico = (data) => {
   const splittedDate = data.fecha.split("/");
 
@@ -30,8 +34,10 @@ const formatBanxico = (data) => {
   };
 }
 
+// Get fixer data from the API and send it back to the user
 const getFixer = async () => {
   try {
+    // Makes the request to the API server with the data we want to receive
     const result = await axios.get("http://data.fixer.io/api/latest", {
       params: {
         access_key: process.env.FIXER_TOKEN,
@@ -40,6 +46,7 @@ const getFixer = async () => {
       }
     });
     
+    // Return formatted information
     return formatFixer(result.data);
   } catch (error) {
     console.log(error);
@@ -48,7 +55,9 @@ const getFixer = async () => {
   }
 }
 
+// Get Diario Oficial data from the web page doing some web scrapping
 const getDiarioOficial = async () => {
+  // Define options to connect to the web page and download the content
   const options = {
     uri: "http://www.banxico.org.mx/tipcamb/tipCamMIAction.do",
     transform: function (body) {
@@ -57,12 +66,15 @@ const getDiarioOficial = async () => {
   };
 
   try {
+    // Get an object and stored in a jQuery-ish variable to use the smae selectors
     const $ = await rp(options);
   
     let date;
     let value;
     const rowNon = $(".renglonNon");
   
+    // Get the first row of the table to get the data thend find all
+    // the <td> elements and get the data from the ones we are interested on
     rowNon.find("td").each(function(index, elem) {
       if (index === 0) {
         date = $(this).text().replace(/\n| /g,"").trim();
@@ -71,6 +83,7 @@ const getDiarioOficial = async () => {
       }
     });
   
+    // Format data and send it back to the client
     return formatDiarioOficial(value, date);
   } catch (error) {
     console.log(error);
@@ -79,8 +92,11 @@ const getDiarioOficial = async () => {
   }
 }
 
+// Get Banxico information from the API and send it back to the client
 const getBanxico = async () => {
   try {
+
+    // Make the request with the token from Banxico API
     const result = await axios.get("https://www.banxico.org.mx/SieAPIRest/service/v1/series/sf43718//datos/oportuno", {
       params: {
         token: process.env.BANXICO_TOKEN
@@ -89,6 +105,7 @@ const getBanxico = async () => {
 
     const data = result.data.bmx.series[0].datos[0];
 
+    // Format data and send it back to the client
     return formatBanxico(data);
   } catch (error) {
     console.log(error);
